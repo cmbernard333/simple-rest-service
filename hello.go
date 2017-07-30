9754524f2714ab6aa404be83b0126ce0cc62b74e
Christian Bernard <cmbernard333@gmail.com>
2017-07-30 13:29:31 -0600

    initial commit of hello.go in package github.com/cmbernard333/hello

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+"fmt"
+"github.com/cmbernard333/stringutil" // grab the package from directory $GOPATH/src/github.com/cmbernard333/stringutil
+)
+
+func main() {
+    fmt.Printf("Hello, world!\n")
+}
