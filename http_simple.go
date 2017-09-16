8b6914f7090f404fd2fc992b6ef455c33407330b
Christian Bernard <cmbernard333@gmail.com>
2017-09-16 10:32:21 -0600

    created a function for assigning a map of handlers to routes to make it simpler to setup. added the beginnings of a rest endpoint called {todoId}.

--- http_simple.go
+++ http_simple.go
@@ -11,12 +11,34 @@ func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w,"Welcome to the Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w,"Todo show:", todoId)
+}
+
+// assign all the functions mapped in the map 'm'
+func SetupRouter(router *mux.Router, m map[string]func(http.ResponseWriter,*http.Request)) {
+    for key, value := range m {
+        router.HandleFunc(key,value)
+    }
+}
+
 func main() {
+    // map paths to handlers - make creates a reference to a map and assigns it to m
+    m := make(map[string]func(http.ResponseWriter,*http.Request));
+    m["/"] = Index
+    m["/todos"] = TodoIndex
+    m["/todos/{todoId}"] = TodoShow
     /*
-    The example creates a basic router,
+    The example creates a router,
     adds the route / and assigns the Index handler to run when that endpoint is called.
     */
     router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/",Index)
+    SetupRouter(router,m)
     log.Fatal(http.ListenAndServe(":8080",router))
 }
